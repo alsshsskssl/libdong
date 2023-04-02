c89b2a43bde4b7d3b22c364044bcbab70d96d963
kdg <alsshsskssl@gmail.com>
2023-04-02 20:02:43 +0900

    add 추억점수

---
+++ C++/memory_score/solution.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+
+    
+    return answer;
+}
+
+int main(){
+    vector<string> name;
+    vector<int> yearning;
+    vector<vector<string>> photo;
+    vector<int> answer;
+    
+    name.push_back("may")
+    name.push_back("kein")
+    name.push_back("kain")
+    name.push_back("radi")
+
+
+    
+    yearning.push_back
+
+
+    return 0;
+}
