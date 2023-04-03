fa5415d455e8e28f6cfddc20ef0ff2a76f2e2bd2
kdg <alsshsskssl@gmail.com>
2023-04-03 21:51:51 +0900

    add solution

---
+++ C++/project_assignment/solution.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// plans_rows는 2차원 배열 plans의 행 길이, plans_cols는 2차원 배열 plans의 열 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char** solution(const char*** plans, size_t plans_rows, size_t plans_cols) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char** answer = (char**)malloc(1);
+    return answer;
+}
